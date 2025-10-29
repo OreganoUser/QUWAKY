00fe1a9253086eecdd6a486f237582ab4cc52b29
OreganoUser <lejeunecharel@icloud.com>
2025-10-29 20:51:12 +0100

    Add seasons

--- Motoscope/motoscope-api/seeds/2025-10-28_001_seasons_base.sql
+++ Motoscope/motoscope-api/seeds/2025-10-28_001_seasons_base.sql
@@ -1,9 +1,9 @@
 BEGIN;
+INSERT INTO seasons (id, year, created_at)
+VALUES (1, 2025, now())
+ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO seasons (year) VALUES (2024)
-ON CONFLICT (year) DO NOTHING;
-
-INSERT INTO seasons (year) VALUES (2025)
-ON CONFLICT (year) DO NOTHING;
-
+INSERT INTO seasons (id, year, created_at)
+VALUES (2, 2024, now())
+ON CONFLICT (id) DO NOTHING;
 COMMIT;
