2d34c3680ce6a98e6ceb0d310b2062c37302ec53
OreganoUser <lejeunecharel@icloud.com>
2025-10-29 21:23:43 +0100

    Aight now fr though

--- Motoscope/motoscope-api/seeds/2025-10-28_002_circuits_base.sql
+++ Motoscope/motoscope-api/seeds/2025-10-28_002_circuits_base.sql
@@ -1,11 +1,9 @@
 BEGIN;
-
 INSERT INTO circuits (slug, name, country, length_km, laps)
 VALUES ('losail','Lusail International Circuit','Qatar',5.380,22)
-ON CONFLICT DO NOTHING
-SET name=EXCLUDED.name,
-    country=EXCLUDED.country,
-    length_km=EXCLUDED.length_km,
-    laps=EXCLUDED.laps;
-
+ON CONFLICT (slug) DO UPDATE
+SET name = EXCLUDED.name,
+    country = EXCLUDED.country,
+    length_km = EXCLUDED.length_km,
+    laps = EXCLUDED.laps;
 COMMIT;
