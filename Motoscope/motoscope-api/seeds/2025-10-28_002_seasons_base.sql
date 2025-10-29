b6a0293374525c10b6fb43586fe7f0f93d1715f3
OreganoUser <lejeunecharel@icloud.com>
2025-10-29 21:06:57 +0100

    delete sql file

--- Motoscope/motoscope-api/seeds/2025-10-28_002_seasons_base.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-INSERT INTO seasons (id, year, created_at)
-VALUES (1, 2025, now())
-ON CONFLICT (id) DO NOTHING;
-
-INSERT INTO seasons (id, year, created_at)
-VALUES (2, 2024, now())
-ON CONFLICT (id) DO NOTHING;
-COMMIT;
